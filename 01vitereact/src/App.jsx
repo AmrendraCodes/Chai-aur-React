3be4b0fa06b845aa7ff5d4a51656e2ec3b484a3c
amrendra1998 <techupdate855@gmail.com>
2025-06-15 08:54:18 +0530

    file committed

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -2,9 +2,13 @@ import Chai from "./chai"
 
 
 function App() {
+  const username = "chai aur code"
 return (
+  <>
   <Chai/>
-
+  <h1>chai aur react {username}</h1>
+  <p>test para</p>
+  </>
 )
 
   
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -2,9 +2,35 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
 import App from './App.jsx'
+import React from 'react'
+
+function MyApp() {
+    return(
+        <div>
+            <h1>My custom App!!!</h1>
+        </div>
+    )
+}
+// const reactElement = {
+//     type: 'a',
+//     props: {
+//         href: 'https://google.com',
+//         target: '_blank'
+//     },
+//     children: 'Click me to visit Google'
+// };
+
+const anotheruser = "chai aur react"
+const reactElement = React.createElement(
+    'a',
+    {href: 'https://google.com', target: '_blank'},
+    'click me visit to google',
+      anotheruser
+)
+
+
+
 
 createRoot(document.getElementById('root')).render(
  
-    <App />
-  
-)
+reactElement )
