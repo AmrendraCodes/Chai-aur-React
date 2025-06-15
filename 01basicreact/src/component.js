156bea1b2a5db9900691a1ad04fba8470d9b9ba8
amrendra1998 <Amrendra1999official@gmail.com>
2025-06-15 23:02:04 +0530

    file commited

---
+++ 01basicreact/src/component.js
@@ -0,0 +1,7 @@
+function Component() {
+  return (
+    <h2>This is my new component!</h2>
+  );
+}
+
+export default Component;
---
+++ 02_Counter_App/src/component.jsx
@@ -0,0 +1,35 @@
+
+import { useState } from 'react'
+function Component() {
+
+  let [counter, setCounter] = useState(0)
+  // let counter = 15
+  const addValue  = () => {
+    if (counter < 20) {
+      setCounter(counter+1);
+      
+    }
+
+    // counter = counter + 1
+   
+
+  }
+
+   const removeValue = () => {
+    if (counter > 0) {
+      setCounter(counter - 1 )
+    }
+    
+   }
+  return (
+    <>
+<h1>Our React APP</h1> 
+<h3>Counter value : {counter}</h3>
+<button onClick={addValue}>Add value{counter} </button>
+<button onClick={removeValue}>Remove value {counter}</button>
+<p> footer: {counter}</p>
+</>
+);
+}
+
+export default Component;
