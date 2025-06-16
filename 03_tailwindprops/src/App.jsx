e528babb93429c6b5bd6f021ab6ee95fc7b014d5
amrendra1998 <Amrendra1999official@gmail.com>
2025-06-16 23:34:10 +0530

    file commited

---
+++ 03_tailwindprops/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="text-center p-10">
+      <h1 className="text-4xl font-bold text-blue-600">Hello Tailwind + Vite!</h1>
+    </div>
+  );
+}
+
+export default App
